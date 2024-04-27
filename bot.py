707a641ddb95b8a8f050c4f989612c34d41c4898
Nabi <nabi.usonov@gmail.com>
2024-04-27 13:10:37 +0500

    Fixed broken commit

--- bot.py
+++ bot.py
@@ -4,14 +4,12 @@ import logging
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import (
     ApplicationBuilder,
-    ApplicationBuilder,
     CommandHandler,
     ContextTypes,
     Application,
     ConversationHandler,
     MessageHandler,
     filters
-)
 )
 import sqlite3
  
@@ -24,17 +22,14 @@ logging.basicConfig(
  
 logger = logging.getLogger(__name__)
  
- 
 GENDER, SUBJECTSTRONG, SUBJECTWEAK, BIO = range(4)
  
  
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
     if db_find_user(user.username):
-    if db_find_user(user.username):
         await update.message.reply_text(
             f'Hello {user.first_name}! Glad to see you again'
-        )
         )
     else:
         context.user_data['user_id'] = user.id
@@ -46,7 +41,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             f'Hello! Let\'s get you a study partner.'
             'Send /cancel to stop talking to me.\n\n'
             'Are you male of a female?',
-            reply_markup=ReplyKeyboardMarkup(
             reply_markup=ReplyKeyboardMarkup(
                 reply_keyboard, one_time_keyboard=True, input_field_placeholder='Male or Female?'
             )
@@ -67,11 +61,9 @@ async def get_gender(update: Update, context: ContextTypes.DEFAULT_TYPE):
  
     await update.message.reply_text(
         'Wonderful! Now let\'s choose a subject you are skilled in. ',
-        'Wonderful! Now let\'s choose a subject you are skilled in. ',
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Choose your strong subject.'
         ))
-    
     
     return SUBJECTSTRONG
  
@@ -87,7 +79,6 @@ async def get_subjectstrong(update: Update, context: ContextTypes.DEFAULT_TYPE):
  
     await update.message.reply_text(
         'Great! Now let\'s choose your lacking subject',
-        reply_markup=ReplyKeyboardMarkup(
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Choose your weak subject.'
         )
@@ -124,13 +115,7 @@ async def get_bio(update: Update, context: ContextTypes.DEFAULT_TYPE):
                           userdata['user_bio'], userdata['subject_strong'], userdata['subject_weak'], userdata['user_gender'])
         logger.info(
             f"User: {userdata['username']} added to the db succesfully.")
-        db_table_add_user(userdata['user_id'], userdata['user_name'], userdata['user_surname'], userdata['username'],
-                          userdata['user_bio'], userdata['subject_strong'], userdata['subject_weak'], userdata['user_gender'])
-        logger.info(
-            f"User: {userdata['username']} added to the db succesfully.")
     except Exception as e:
-        logger.error(
-            f'Failed to add user: {userdata["username"]} to the db. ERROR: {e}')
         logger.error(
             f'Failed to add user: {userdata["username"]} to the db. ERROR: {e}')
     return ConversationHandler.END
@@ -201,8 +186,6 @@ def db_delete_user(user_id: int):
 def db_table_add_user(user_id: int, user_name: str, user_surname: str, username: str, user_bio: str, subject_strong: str, subject_weak: str, user_gender: str):
     cursor.execute('INSERT INTO chatusers (user_id, user_name, user_surname, username, user_bio, subject_strong, subject_weak, user_gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
                    (user_id, user_name, user_surname, username, user_bio, subject_strong, subject_weak, user_gender))
-    cursor.execute('INSERT INTO chatusers (user_id, user_name, user_surname, username, user_bio, subject_strong, subject_weak, user_gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                   (user_id, user_name, user_surname, username, user_bio, subject_strong, subject_weak, user_gender))
     conn.commit()
  
  
@@ -216,6 +199,7 @@ def db_table_edit_user(user_id: int, db_key: str, db_value):
         return None
  
 def db_matched_users(subject_strong: str, subject_weak: str):
+    print(f"SELECT * FROM chatusers WHERE subject_strong = '{subject_weak}' AND subject_weak = '{subject_strong}'")
     users_found = cursor.execute(f"SELECT * FROM chatusers WHERE subject_strong = '{subject_weak}' AND subject_weak = '{subject_strong}'")
     users_found = users_found.fetchall()
     if len(users_found) > 0:
@@ -247,7 +231,6 @@ if __name__ == '__main__':
         '6593288094:AAEaoKHSXV_sQDGSj6C2W9lCF6bjL5f3Bmg').build()
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
-        entry_points=[CommandHandler('start', start)],
         states={
             GENDER: [MessageHandler(filters.Regex('^(Male|Female)$'), get_gender)],
             SUBJECTSTRONG: [MessageHandler(filters.Regex('^(Algebra|Calculus|Programming|Chemistry|History|Philosophy|Physics|Economics)$'), get_subjectstrong)],
@@ -256,12 +239,9 @@ if __name__ == '__main__':
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
     app.add_handler(conv_handler)
     app.add_handler(CommandHandler('edit_info', edit_info))
     app.add_handler(CommandHandler('me', show_user))
     app.add_handler(CommandHandler('match', match_user))
     app.add_handler(MessageHandler(filters.COMMAND, unknown))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
-
